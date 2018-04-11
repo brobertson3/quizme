6962b7e9ebb07df55f5df5c8d3e1db91557cbbbb
brobertson3 <brobertson3@users.noreply.github.com>
2018-04-11 13:57:31 -0700

    Finished the logicfor the game board.
    Added all of the questions and answers for the game. Also added the logic to change the prompts based on the choice selected. Added links to my social media and portfolio with font awesome icons. All buttons are hidden/shown when they're supposed to.

--- js/index.js
+++ js/index.js
@@ -4,8 +4,14 @@
 const speechBubble = {
 	_questionNumber: 1,
 	_correct: '',
-	_greeting: "Let's see how much you know about this great guy.",
-	_question: `Ready for question `,
+	_greeting: "How's it going? I hope you're ready to learn a little about me. Press the Start Game button to begin!",
+	_question: 'Are you ready for Question ',
+	_correctResponse: ' was the right answer. Nicely done!',
+	_wrongResponse: "Oh no! That's not quite right. What you were looking for was choice ",
+	_checkScorePrompt: "All finished! Click the 'Check Score' button to the right to see how you did.",
+	_allCorrect: "Wow, you got them all right! Either you know me pretty well or you're a whiz at multiple choice. Either way, there's much more to learn about me. Click one of the links to the right to check out my portfolio and social media. Or you can play again!",
+	_someWrongBeginning: "Uh oh. Looks like you got ",
+	_someWrongEnding: " wrong. Don't fret. We can change that. Just check out the links to the right to view my portfolio and social media accounts. Or if you want, you can play again!",
 	set questionNumber(number) {
 		if (typeof number === 'number') {
 			this._questionNumber = number;
@@ -16,8 +22,24 @@ const speechBubble = {
 	get questionNumber() {
 		return this._questionNumber;
 	},
-	_correctResponse: `Yes, ${this._correct} was the right answer.`,
-	_wrongResponse: `Oh no! That's not quite right. ${this._correct} is what you were looking for.`,
+	get question() {
+		return this._question + this._questionNumber + '?';
+	},
+	get correctResponse() {
+		return this._correct + this._correctResponse;
+	},
+	get wrongResponse() {
+		return this._wrongResponse + this._correct;
+	},
+	get checkScorePrompt() {
+		return this._checkScorePrompt;
+	},
+	get allCorrect() {
+		return this._allCorrect;
+	},
+	get someWrong() {
+		return this._someWrongBeginning + (4 - questionAnswer.totalScore) + this._someWrongEnding;
+	},
 	set correct(letter) {
 		if (letter === 'A' || letter === 'B' || letter === 'C' || letter === 'D') {
 			this._correct = letter;
@@ -37,14 +59,14 @@ const speechBubble = {
  const questionAnswer = {
  	_totalScore: 0,
  	_numberOfQuestions: 4,
- 	_questionOne: ["Question One", "A. Something goes here", "B. Something goes here",
- 	               "C. Something goes here", "D. Something goes here", "A"],
- 	_questionTwo: ["Question Two", "A. Something goes here", "B. Something goes here",
- 	               "C. Something goes here", "D. Something goes here", "B"],
- 	_questionThree: ["Question Three", "A. Something goes here", "B. Something goes here",
- 	               "C. Something goes here", "D. Something goes here", "C"],
- 	_questionFour: ["Question Four", "A. Something goes here", "B. Something goes here",
- 	               "C. Something goes here", "D. Something goes here", "D"],
+ 	_questionOne: ["Where does Brent currently live?", "A. Brea, CA", "B. Baltimore, MD",
+ 	               "C. Seattle, WA", "D. Portland, OR", "A"],
+ 	_questionTwo: ["What languages is Brent experienced in?", "A. HTML5", "B. CSS3",
+ 	               "C. JavaScript", "D. All of the above", "D"],
+ 	_questionThree: ["What position is Brent currently looking to fill.", "A. Web Developer", "B. UI Developer",
+ 	               "C. UI/UX Designer", "D. All of the above", "D"],
+ 	_questionFour: ["When is Brent available to start?", "A. Not looking", "B. Immediately",
+ 	               "C. A few months", "D. Next year", "B"],
  	get totalScore () {
  		return this._totalScore;
  	},
@@ -66,7 +88,6 @@ const speechBubble = {
  	get questionFour () {
  		return this._questionFour;
  	}
-
  };
 
 // Starts off everything
@@ -82,11 +103,11 @@ const init = () => {
 const newGame = () => {
 	speechBubble.questionNumber = 1;
 	questionAnswer.totalScore = 0;
-	document.querySelector('.left-pane').classList.add('hidden');
-	document.querySelector('.right-pane').classList.add('hidden');
+	// document.querySelector('.left-pane').classList.add('hidden');
+	// document.querySelector('.right-pane').classList.add('hidden');
 	document.querySelector('.score-area').classList.add('hidden');
-	document.querySelector('.question-area').classList.remove('hidden');
-	document.querySelector('.choices-area').classList.remove('hidden');
+	// document.querySelector('.question-area').classList.remove('hidden');
+	// document.querySelector('.choices-area').classList.remove('hidden');
 	document.getElementById('start-btn').classList.remove('hidden');
 	$('.choice-btn').removeClass('correct incorrect disabled');
 	$('.choice-btn').click(evaluateAnswer);
@@ -98,9 +119,13 @@ const newGame = () => {
  * button disappear and everything else visible.
  */ 
 const startGame = () => {
+	document.getElementById('speech-text').textContent = speechBubble.question;
 	document.getElementById('start-btn').classList.add('hidden');
-	document.querySelector('.right-pane').classList.remove('hidden');
-	document.querySelector('.left-pane').classList.remove('hidden');
+	document.querySelector('.question-area').classList.remove('hidden');
+	$('.choices-area').removeClass('hidden');
+
+	// document.querySelector('.right-pane').classList.remove('hidden');
+	// document.querySelector('.left-pane').classList.remove('hidden');
 }
 
 // // Start game
@@ -122,6 +147,8 @@ const nextQuestion = () => {
 	// On click of an answer, evaluate whether correct
 	$('.choice-btn').click(evaluateAnswer);
 	$('.choice-btn').removeClass('incorrect correct disabled');
+	document.getElementById('speech-text').textContent = speechBubble.question;
+	document.getElementById('next-btn').classList.add('hidden');
 };
 
 const checkScore = () => {
@@ -132,6 +159,11 @@ const checkScore = () => {
 	document.getElementById('check-score-btn').classList.add('hidden');
 	document.querySelector('.score-area').classList.remove('hidden');
 	document.getElementById('score').textContent = `${percentage}%`;
+	if (questionAnswer.totalScore < 4) {
+		document.getElementById('speech-text').textContent = speechBubble.someWrong;
+	}  else {
+		document.getElementById('speech-text').textContent = speechBubble.allCorrect;
+	}
 };
 
 const evaluateAnswer = event => {
@@ -160,15 +192,18 @@ const evaluateAnswer = event => {
 	// Add the disabled class, which will stop the hover effect
 	//TODO need to make the color stay the same as before though
 	$('.choice-btn').addClass('disabled');
-
+	//Set the correct variable for the speech bubble to use
+	speechBubble.correct = questionAnswer[`question${curr}`][5];
 	// If the correct answer is chosen, add point to score.
 	if (event.target.id[7].toUpperCase() === questionAnswer[`question${curr}`][5]) {
-		console.log('That is correct');
+		// console.log('That is correct');
 		questionAnswer.totalScore = questionAnswer.totalScore + 1;
 		event.target.classList.add('correct');
+		document.getElementById('speech-text').textContent = speechBubble.correctResponse;
 	} else {
-		console.log("This is incorrect.");
+		// console.log("This is incorrect.");
 		event.target.classList.add('incorrect');
+		document.getElementById('speech-text').textContent = speechBubble.wrongResponse;
 	}
 
 	if (curr !== 'Four') {
@@ -177,10 +212,11 @@ const evaluateAnswer = event => {
 		speechBubble.questionNumber++;
 
 	} else {
-		document.getElementById('next-btn').classList.add('hidden');
+		// document.getElementById('next-btn').classList.add('hidden');
 		// Show the check score button
 		document.getElementById('check-score-btn').classList.remove('hidden');
-		document.getElementById('check-score-btn').addEventListener('click', checkScore);
+		document.getElementById('speech-text').textContent += ' ' + speechBubble.checkScorePrompt;
+		// document.getElementById('check-score-btn').addEventListener('click', checkScore);
 	}
 	
 }
@@ -214,7 +250,7 @@ $('#next-btn').click(function () {
 	nextQuestion();
 });
 $('#new-game-btn').click(newGame);
-
+document.getElementById('check-score-btn').addEventListener('click', checkScore);
 /* TODO 
  * A.
  * x1. Calculate the score percentage when 'Check Score' pressed.
@@ -231,10 +267,10 @@ $('#new-game-btn').click(newGame);
  */
 
 
-// let newObj = speechBubble;
-// newObj.correct = 'A';
-// console.log(newObj.correct);
-
+ // let newObj = speechBubble;
+ // newObj.correct = 'A';
+ // console.log(newObj.correct);
+ // console.log(newObj.correctResponse);
 // newObj.questionNumber = 3;
 // console.log(newObj.questionNumber);
 // console.log(newObj._question + newObj.questionNumber + '?');
